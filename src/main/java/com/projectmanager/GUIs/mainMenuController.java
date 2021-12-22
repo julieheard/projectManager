b94b7b3f2c7895323b60419d9afe1e5c801fdb42
Julie <Julie@users-noreply.github.com>
2021-12-22 17:47:20 +0000

    Added CSS and exit button fixed

--- src/main/java/com/projectmanager/GUIs/mainMenuController.java
+++ src/main/java/com/projectmanager/GUIs/mainMenuController.java
@@ -1,4 +1,16 @@
 package com.projectmanager.GUIs;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class mainMenuController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    protected void exit(){
+        System.exit(0);
+    }
+
 }
